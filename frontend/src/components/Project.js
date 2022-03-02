5de3f44a3e7e5ffc429b2443df6fcc8c3e9e7590
Cristian Muscalu <muscaiu@yahoo.com>
2022-03-02 19:08:24 +0200

    showing projects in homepage

--- frontend/src/components/Project.js
+++ frontend/src/components/Project.js
@@ -4,33 +4,30 @@ import Image from "gatsby-image"
 import { FaGithubSquare, FaShareSquare } from "react-icons/fa"
 import { graphql, useStaticQuery } from "gatsby"
 
-const Project = () => {
-  // const data = useStaticQuery(query)
-  // console.log("data:", data)
-
-  // const query = graphql`
-  //   {
-  //     allStrapiProjects(
-  //       filter: {
-  //         data: { elemMatch: { attributes: { featured: { eq: true } } } }
-  //       }
-  //     ) {
-  //       nodes {
-  //         data {
-  //           attributes {
-  //             description
-  //             github
-  //             title
-  //             url
-  //             featured
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // `
-
-  return <h2>project copmonent</h2>
+const Project = ({ description, title, github, url, image, stack, index }) => {
+  return (
+    <article className="project">
+      <Image fluid={image.childImageSharp.fluid} className="project-img" />
+      <div className="project-info">
+        <div className="project-number">0{index + 1}</div>
+        <h3>{title}</h3>
+        <p className="project-desc">{description}</p>
+        <div className="project-stack">
+          {stack.map(item => {
+            return <span key={item.id}>{item.name}</span>
+          })}
+        </div>
+        <div className="project-links">
+          <a href={github}>
+            <FaGithubSquare className="project-icon" />
+          </a>
+          <a href={url}>
+            <FaShareSquare className="project-icon" />
+          </a>
+        </div>
+      </div>
+    </article>
+  )
 }
 
 Project.propTypes = {}
