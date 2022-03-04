d8b1bc5cc440927138d6ad041cc52d7d226d8821
Cristian Muscalu <muscaiu@yahoo.com>
2022-03-04 19:45:12 +0200

    twitter card preview

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Gatsby portofolio with strapi back-end
+
+## Twitter dev card previewer tool
+
+https://cards-dev.twitter.com/validator
--- frontend/src/components/SEO.js
+++ frontend/src/components/SEO.js
@@ -37,9 +37,12 @@ const SEO = ({ title, description }) => {
       <meta name="description" content={description || siteDesc} />
       <meta name="image" content={image} />
       <meta name="author" content={author} />
-      <meta name="author" content={author} />
-      <meta name="siteUrl" content={siteUrl} />
-      <meta name="twitterUsername" content={twitterUsername} />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content={twitterUsername} />
+      <meta name="twitter:title" content={siteTitle} />
+      <meta name="twitter:description" content={siteDesc} />
+      <meta name="twitter:image" content={`${siteUrl}${image}`} />
     </Helmet>
   )
 }
